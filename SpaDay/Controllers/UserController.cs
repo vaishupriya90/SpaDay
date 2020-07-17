1cc618a9f3830df34fb4d9b2bc0de6e0c1f9557a
Vaishu <vaishupriya90@gmail.com>
2020-07-17 17:01:01 -0500

    created and updated user controller

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/user/add/")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("/user/add/")]
+        public IActionResult SubmitAddUserForm(User newUser,string verify)
+        {
+            string email = newUser.EmailAddress;
+            if (newUser.Password.Equals(verify))
+            {
+                ViewBag.userName = newUser.UserName;
+                return View("Index");
+            }
+            ViewBag.error = "ERROR: The Passwords should match";
+            ViewBag.usrName = newUser.UserName;
+            ViewBag.email = newUser.EmailAddress;
+            
+            return View();
+            
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        //public string verify { get; set; }
+        
+        public User()
+        {
+        }
+    }
+}
